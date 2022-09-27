41ca453c55a2d228babaad18d64ed2c519f9bb50
田爪大智 <>
2022-09-28 08:22:23 +0900

    webpacker:install の実行 create, new, index, show の作成 routes.rb への追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_27_003008) do
+ActiveRecord::Schema.define(version: 2022_09_27_025300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sample_articles", force: :cascade do |t|
     t.string "title"
