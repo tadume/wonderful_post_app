a2d74ded649f17a8010e1439f8be747a039428c5
田爪大智 <>
2022-09-29 09:13:15 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'mypage#show'
   devise_for :users
   root to: "articles#index"
-  resources :sample_articles
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
