656d7940a95e1784e8844ee64bba27337650151d
田爪大智 <>
2022-09-29 13:40:28 +0900

    seedファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 2022_09_28_104941) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "sample_articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
